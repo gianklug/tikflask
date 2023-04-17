d6a09c0148d57cecd984d9125c4c065b4a1771ae
Gian Klug <gian.klug@ict-scouts.ch>
2023-04-17 20:16:14 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3
+
+import os
+import time
+
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, redirect, render_template, request
+from opengraph import opengraph
+
+TIKTOK_USERNAME = os.getenv("TIKTOK_USERNAME")
+
+
+class Video:
+    def __init__(self, data: BeautifulSoup):
+        self.data = data
+        self.banner = self.get_banner()
+        self.url = self.get_url()
+        self.description = self.get_description()
+
+    def get_banner(self):
+        return (
+            self.data.find("div", {"class": "tiktok-1jxhpnd-DivContainer"})
+            .find("img")
+            .get("src")
+        )
+
+    def get_url(self):
+        return (
+            self.data.find("div", {"class": "tiktok-yz6ijl-DivWrapper"})
+            .find("a")
+            .get("href")
+        )
+
+    def get_description(self):
+        return (
+            self.data.find("div", {"class": "tiktok-1jxhpnd-DivContainer"})
+            .find("img")
+            .get("alt")
+        )
+
+
+class User:
+    def __init__(self, username, image, bio):
+        self.username = username
+        self.image = image
+        self.bio = bio.split(" | ")[1].split(" Followers")[0] + " Followers"
+
+
+def get_user_info():
+    url = f"https://www.tiktok.com/@{TIKTOK_USERNAME}"
+    data = opengraph.OpenGraph(url=url)
+    return User(TIKTOK_USERNAME, data["image"], data["description"])
+
+
+def get_videos():
+    base_url = f"https://www.tiktok.com/@{TIKTOK_USERNAME}"
+
+    # get the html
+    data = requests.get(base_url).text
+    # parse the html
+    soup = BeautifulSoup(data, "html.parser")
+
+    # get all `user-post-item` divs
+    posts = soup.find_all("div", {"data-e2e": "user-post-item"})
+
+    videos = []
+    # print
+    for post in posts:
+        videos.append(Video(post))
+
+    return videos
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def loading():
+    return render_template("loading.html")
+
+
+@app.route("/index")
+def index():
+    return render_template("index.html", videos=get_videos(), user=get_user_info())
+
+
+@app.route("/download", methods=["POST"])
+def download_url():
+    url = request.form.get("url")
+    r = requests.post("https://snaptik.io/download/parse-url", data={"url": url}).json()
+    while not r["code"] == 3:
+        time.sleep(1)
+        r = requests.post(
+            "https://snaptik.io/download/parse-url", data={"url": url}
+        ).json()
+
+    return redirect(r["url"])
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
